e71147f8dfe29c9765e3f672fd8df4a786419e86
zouhirmahjoubi <zouhir.mahjoubi@gmail.com>
2026-06-10 22:11:11 +0100

    clean: remove duplicate app folder and archive

--- app/apps/api/src/constants/common.js
+++
@@ -1,8 +0,0 @@
-const NodeEnv = {
-	Development: 'development',
-	Production: 'production',
-};
-
-const BodyLimit = 1024 * 1024 * 20;
-
-export { NodeEnv, BodyLimit };
--- app/apps/web/src/lib/pocketbaseClient.js
+++
@@ -1,9 +0,0 @@
-import Pocketbase from 'pocketbase';
-
-const POCKETBASE_API_URL = '/hcgi/platform';
-
-const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
-
-export default pocketbaseClient;
-
-export { pocketbaseClient };
